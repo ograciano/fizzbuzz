9992ff7df52b1d5aacc2bfcc50db2abf9ceb5978
ograciano <cev3067@innovaccion.mx>
2022-04-26 21:53:41 -0500

    gitactions created

---
+++ app/lib/services/ExplorerService.js
@@ -0,0 +1,20 @@
+
+
+class ExplorerService {
+    static filterByMission(explorers = [] , mission = '') {
+        return explorers.filter((explorer) => explorer.mission == mission);
+    }
+    
+    static getAmountOfExplorersByMission(explorers = [], mission) {
+        const amountExplorers = explorers.filter((explorer) => explorer.mission == mission);
+        return amountExplorers.length;
+    }
+    
+    static getExplorersUsernamesByMission(explorers = [], mission = '') {
+        const explorersByMission = this.filterByMission(explorers, mission);
+        return explorersByMission.map((explorer) => explorer.githubUsername);
+
+    }
+}
+
+module.exports = ExplorerService;
